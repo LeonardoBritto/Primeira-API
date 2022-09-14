a83660b2f2e40d10cf4123fe9bc28f2f6ec680f4
Leonardo Brito <leonardobrito992@gmail.com>
2022-09-13 23:31:00 -0300

    Commmit Teste

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./config/routes')
 
-
 const app = express()
-
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended : false}))
